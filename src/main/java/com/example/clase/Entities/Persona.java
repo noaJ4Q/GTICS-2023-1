5d7e2a2b4cce44432aa35b588a6335a7aa712686
noe <noe.jaraq@gmail.com>
2023-04-01 11:27:12 -0500

    New entity added

---
+++ src/main/java/com/example/clase/Entities/Persona.java
@@ -0,0 +1,40 @@
+package com.example.clase.Entities;
+
+public class Persona {
+    private String name;
+    private String surname;
+    private String nacionality;
+    private String dni;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getNacionality() {
+        return nacionality;
+    }
+
+    public void setNacionality(String nacionality) {
+        this.nacionality = nacionality;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
